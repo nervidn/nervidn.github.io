69b1ffec578dc8657e855b756039e6c2fe438428
Rinha242 <randish.dev@gmail.com>
2024-04-24 14:26:09 +0700

    update

--- hal/profile/script.js
+++ hal/profile/script.js
@@ -1,8 +1,8 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "25%";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0%";
 }
